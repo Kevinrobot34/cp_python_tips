a03a4b117c24992fde491f7e2af89b0a9796f1f7
Kosaku Ono <kevinrobot34@yahoo.co.jp>
2020-10-01 17:51:39 +0900

    Add codes for a techblog

---
+++ README.md
@@ -0,0 +1,8 @@
+## PyCon JP 2020
+`pycon2020/`のディレクトリは[PyCon JP 2020]( https://pycon.jp/2020/ )での発表資料制作に使ったコード置き場です。
+
+* [Slide](https://www.slideshare.net/ssuserf0844f/pycon2020-238316030)
+* [YouTube](https://www.youtube.com/watch?v=nboiFJ8A6_I)
+
+
+## techblog
---
+++ techblog/sort.py
@@ -0,0 +1,77 @@
+import timeit
+from random import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def func1(a):
+    a.sort(key=lambda x: x[1])
+
+
+def func2(a):
+    from operator import itemgetter
+    a.sort(key=itemgetter(1))
+
+
+def main():
+    n_repeat = 10
+    n = 10**6
+    a = [[random(), random(), random()] for i in range(n)]
+    res1 = timeit.repeat(lambda: func1(a), repeat=n_repeat, number=1)
+    res1 = np.array(res1) * 10**3
+    res2 = timeit.repeat(lambda: func2(a), repeat=n_repeat, number=1)
+    res2 = np.array(res2) * 10**3
+
+    print(np.mean(res1), np.std(res1))
+    print(np.mean(res2), np.std(res2))
+
+    # Example data
+    y_pos = np.arange(2)
+    performance = [np.mean(res1), np.mean(res2)]
+    error = [np.std(res1), np.std(res2)]
+
+    # plot
+    plt.rcdefaults()
+    fig, ax = plt.subplots(figsize=(10, 4))
+
+    ax.barh(y_pos,
+            performance,
+            xerr=error,
+            align='center',
+            height=0.5,
+            color=['#41aac7', 'red'])
+    ax.text(np.mean(res1) / 2,
+            0,
+            f'{int(np.mean(res1))}ms',
+            fontsize=16,
+            ha='center',
+            va='center',
+            color='black')
+    ax.text(np.mean(res2) / 2,
+            1,
+            f'{int(np.mean(res2))}ms',
+            fontsize=16,
+            ha='center',
+            va='center',
+            color='black',
+            weight='bold')
+
+    ax.grid(axis='y')
+    ax.set_ylim(-0.5, 1.5)
+    ax.set_axisbelow(True)
+    ax.tick_params(axis='x', labelsize=14)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(
+        ['cp_python_list_sort01.py', 'cp_python_list_sort02.py'])
+    # ax.yaxis.set_ticks_position('none')
+    ax.set_xlabel('time[ms]')
+
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig('techblog/sort.png', dpi=200)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
